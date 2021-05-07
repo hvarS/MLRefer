a7966a140606e3cb565cb9bf47dd18ce04dd001f
hvarS <34151780+hvarS@users.noreply.github.com>
2021-05-07 11:08:13 +0530

    Create easykaggleapi_for_datasets.py
    Colab integration for Kaggle Datasets

---
+++ easykaggleapi_for_datasets.py
@@ -0,0 +1,15 @@
+def setup_dataset(text):
+  import shutil
+  import os
+  %cd /content/gdrive/My Drive/Kaggle/
+  strings = text.split(' ')[-1]
+  folder = strings.split('/')[1]
+  os.environ['KAGGLE_CONFIG_DIR'] = "/content/gdrive/My Drive/Kaggle/"+folder
+  print(strings)
+  print(folder)
+  !mkdir $folder
+  shutil.copy2("kaggle.json","./"+folder+"/kaggle.json")
+  %cd $folder
+  !kaggle datasets download -d $strings
+  
+setup_dataset("kaggle datasets download -d luizfmatos/reddit-english-depression-related-submissions")
